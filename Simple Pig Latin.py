486b01ca09a34494462736e9885fac24d4b66f78
Naveendinula <59760329+Naveendinula@users.noreply.github.com>
2021-07-26 10:36:34 +0530

    Update Simple Pig Latin.py

--- Simple Pig Latin.py
+++ Simple Pig Latin.py
@@ -1,3 +1,8 @@
+#Simple Pig Latin
+##Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+
+
 def pig_it(text):
     punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     new_text = text.split(" ")
